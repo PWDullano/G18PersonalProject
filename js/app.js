ad8af0badf8d34cf96f3dc8f36bd1ac21a168177
Patrick Dullano <PWDullano@gmail.com>
2015-12-12 20:44:31 -0700

    made the website responsive

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// http://api.songkick.com/api/3.0/artists/3277856/calendar.json?apikey=tZIW4JSPRTZrAMk3
+$(document).ready(function(){
+  console.log('working')
+})
+
+$('#asubmit').click(function(){
+  console.log('a submitted')
+})
+
+$('#bsubmit').click(function(){
+  console.log('b submitted')
+})
