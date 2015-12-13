26b5acc1a4a8124958aae804525265ab735ef68c
Patrick Dullano <PWDullano@gmail.com>
2015-12-13 12:28:25 -0700

    added functionality to location button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // http://api.songkick.com/api/3.0/artists/3277856/calendar.json?apikey=tZIW4JSPRTZrAMk3
 // http://api.songkick.com/api/3.0/search/artists.json?query={search_query}&apikey={your_api_key}
 var searchArtist;
+var searchLocale;
 var getter;
 var getters;
 
@@ -18,6 +19,7 @@ $('#asubmit').click(function(){
 
   getter.done(function(response){
     var idNumber = response['resultsPage']['results']['artist'][0]['id']
+
     var getters = $.ajax({
       url:'http://api.songkick.com/api/3.0/artists/'+idNumber+'/calendar.json?apikey=tZIW4JSPRTZrAMk3&jsoncallback=?',
       method:'GET',
@@ -25,12 +27,22 @@ $('#asubmit').click(function(){
     })
 
     getters.done(function(responses){
-      var artistInfo = responses['resultsPage']['results']['event'][0]
-      console.log(artistInfo)
+      var artistInfo = responses['resultsPage']['results']['event']
+      console.log(artistInfo[0]['performance'][0])
     })
   })
 })
 
 $('#bsubmit').click(function(){
-  console.log('b submitted')
+  searchLocale = $('#location').val();
+  getter = $.ajax({
+    url:'http://api.songkick.com/api/3.0/search/venues.json?query='+searchLocale+'&apikey=tZIW4JSPRTZrAMk3&jsoncallback=?',
+    method:'GET',
+    dataType:'json'
+})
+
+getter.done(function(response){
+  var locale = response['resultsPage']
+  console.log(locale['results']['venue'])
+  })
 })
