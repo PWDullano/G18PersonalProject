c1f1e5d27eaf24b02a41534c6593a0d931aa7590
Patrick Dullano <PWDullano@gmail.com>
2015-12-16 19:20:15 -0700

    cleaned up code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-// http://api.songkick.com/api/3.0/artists/3277856/calendar.json?apikey=tZIW4JSPRTZrAMk3
-// http://api.songkick.com/api/3.0/search/artists.json?query={search_query}&apikey={your_api_key}
-//
-// http://api.songkick.com/api/3.0/search/locations.xml?query={search_query}&apikey=tZIW4JSPRTZrAMk3
-// http://api.songkick.com/api/3.0/metro_areas/6404/calendar.json?apikey=tZIW4JSPRTZrAMk3
-// http://api.songkick.com/api/3.0/events.json?location=clientip&apikey={your_api_key}&jsoncallback=?
-
-// 'http://developer.echonest.com/api/v4/artist/biographies?api_key=DVMHKXTBO5LYPLMXE&id=songkick:artist:'+idNumber+'&format=json&results=1&start=0'
 var lat;
 var lng;
 var eventInfo;
@@ -17,11 +9,11 @@ $(document).on('click','#appended',function(){
 
 $(document).ready(function(){
   var mapOptions = {
-    center: new google.maps.LatLng(54, -2),
-    zoom: 13,
+    center: new google.maps.LatLng(39.754434,-104.978614),
+    zoom: 3,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 }
-map = new google.maps.Map(document.getElementById('map'), mapOptions)
+    map = new google.maps.Map(document.getElementById('map'), mapOptions)
 });
 
 $('#asubmit').click(function(){
@@ -57,73 +49,10 @@ $('#asubmit').click(function(){
           position: position,
           map: map,
           title: eventInfo
-
         });
-        console.log(lat, lng)
         $('#container').append('<p id="appended">'+results['displayName']+' '+results['start']['time']+'<br>'+results['location']['city']+'<br><a href='+results['uri']+' "</a>Click here for additional event info from Songkick page! '+'</p>');
-        // createMap();
       }
    })
   })
  })
 })
-// var eventInfo = x['displayName']
-//   console.log(eventInfo);
-// var eventVenue = events["venue"]["displayName"]
-// var eventCity = events['location']['city']
-// var eventActs = events['performance'][0]['displayName']
-// var eventLink = events['uri']
-// $('#container').append('<p>'+eventInfo+'</p>')
-// $('#container').append('<p>'+eventVenue+' in '+eventCity+'</p>')
-// $('#container').append('<ul><li>'+eventActs+'</li><ul>')
-// $('#container').append('<a href='+eventLink+' "</a>Click here for additional info from Songkick page! ')
-
-
-
-
-
-
-
-
-
-
-
-// $('#asubmit').click(function(){
-//   searchArtist = $('#artist').val();
-//   getter = $.ajax({
-//     url:'http://api.songkick.com/api/3.0/search/artists.json?query='+searchArtist+'&apikey=tZIW4JSPRTZrAMk3&jsoncallback=?',
-//     method:'GET',
-//     dataType:'json'
-//   });
-//
-//   getter.done(function(response){
-//     var idNumber = response['resultsPage']['results']['artist'][0]['id']
-//
-//     getters = $.ajax({
-//       // url:'http://api.songkick.com/api/3.0/artists/'+idNumber+'/calendar.json?apikey=tZIW4JSPRTZrAMk3&jsoncallback=?',
-//     url:'http://developer.echonest.com/api/v4/artist/biographies?api_key=DVMHKXTBO5LYPLMXE&id=songkick:artist:'+idNumber+'&format=json&results=1&start=0',
-//     method:'GET',
-//     dataType:'json'
-//     })
-//
-//     console.log(idNumber);
-//     getters.done(function(responses){
-//     var artistInfo = responses['response']['biographies'][0]['text']
-//       $('#container').append('<p>'+artistInfo+'</p>')
-//     })
-//   })
-// })
-
-// $('#bsubmit').click(function(){
-//   searchLocale = $('#location').val();
-//   getter = $.ajax({
-//     url:'http://api.songkick.com/api/3.0/search/locations.json?query='+searchLocale+'&apikey=tZIW4JSPRTZrAMk3&jsoncallback=?',
-//     method:'GET',
-//     dataType:'json'
-// })
-//
-// getter.done(function(response){
-//   var locale = response['resultsPage']
-//   console.log(locale['results']['venue'])
-//   })
-// })
